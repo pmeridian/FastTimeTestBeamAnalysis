09785b581684901640e6664377484a3acf8c5eac
Pedro Silva <psilva@cern.ch>
2015-07-17 13:42:51 +0200

    adding x/y reco with TDC

--- interface/H4treeReco.h
+++ interface/H4treeReco.h
@@ -12,10 +12,17 @@ class H4treeReco : public H4tree
  public:
   H4treeReco(TChain *,TString outUrl="H4treeRecoOut.root");
   void Loop(); 
+  void FillTDC();
   ~H4treeReco();
   
  private:
-  
+
+  //TDC readings
+  UInt_t wcXl_, wcXr_, wcYd_, wcYu_;
+  UInt_t MaxTdcChannels_,MaxTdcReadings_;
+  std::vector< std::vector<Float_t> > tdc_readings_;
+  float tdc_recox_, tdc_recoy_;
+
   TTree *recoT_;
   TFile *fOut_;
 };
--- src/H4treeReco.cc
+++ src/H4treeReco.cc
@@ -5,17 +5,31 @@
 #include <iostream>
 
 //
-H4treeReco::H4treeReco(TChain *tree,TString outUrl) : H4tree(tree)
+H4treeReco::H4treeReco(TChain *tree,TString outUrl) : 
+  H4tree(tree),
+  wcXl_(4),   //TDC Xleft
+  wcXr_(5),   //TDC Xright
+  wcYd_(6),   //TDC Ydown
+  wcYu_(7),   //TDC Yup
+  MaxTdcChannels_(16),
+  MaxTdcReadings_(20)
 {
   fOut_  = TFile::Open(outUrl,"RECREATE");
   recoT_ = new TTree("H4treeReco","H4treeReco");
   recoT_->SetDirectory(fOut_);
+
+  //event header
   recoT_->Branch("runNumber",    &runNumber,    "runNumber/i");
   recoT_->Branch("spillNumber",  &spillNumber,  "spillNumber/i");
   recoT_->Branch("evtNumber",    &evtNumber,    "evtNumber/i");
   recoT_->Branch("evtTimeDist",  &evtTimeDist,  "evtTimeDist/i");
   recoT_->Branch("evtTimeStart", &evtTimeStart, "evtTimeStart/i");
   recoT_->Branch("nEvtTimes",    &nEvtTimes,    "nEvtTimes/i");
+
+  //TDC 
+  tdc_readings_.resize(MaxTdcChannels_);
+  recoT_->Branch("tdc_recox", &tdc_recox_, "tdc_recox/F");
+  recoT_->Branch("tdc_recoy", &tdc_recoy_, "tdc_recoy/F");
   
   //add more variables relevant for the study
 
@@ -41,7 +55,7 @@ void H4treeReco::Loop()
       fChain->GetEntry(jentry); 
 
       //save x/y coordinates from the wire chambers
-      //https://github.com/cmsromadaq/H4DQM/blob/master/src/plotterTools.cpp#L1296
+      FillTDC();      
       
       //loop over the relevant channels and reconstruct the waveforms
       //https://github.com/cmsromadaq/H4DQM/blob/master/src/plotterTools.cpp#L1785
@@ -52,6 +66,35 @@ void H4treeReco::Loop()
       recoT_->Fill();
     }
 }
+
+//
+void H4treeReco::FillTDC()
+{
+  tdc_recox_=-999;
+  tdc_recoy_=-999;
+
+  for (uint j=0; j<MaxTdcChannels_; j++){
+    tdc_readings_[j].clear();
+  }
+  
+  for (uint i=0; i<nTdcChannels; i++){
+    if (tdcChannel[i]<MaxTdcChannels_){
+      tdc_readings_[tdcChannel[i]].push_back((float)tdcData[i]);
+    }
+  }
+  
+  if (tdc_readings_[wcXl_].size()!=0 && tdc_readings_[wcXr_].size()!=0){
+    float TXl = *std::min_element(tdc_readings_[wcXl_].begin(),tdc_readings_[wcXl_].begin()+tdc_readings_[wcXl_].size());
+    float TXr = *std::min_element(tdc_readings_[wcXr_].begin(),tdc_readings_[wcXr_].begin()+tdc_readings_[wcXr_].size());
+    tdc_recox_ = (TXr-TXl)*0.005; // = /40./5./10. //position in cm 0.2mm/ns with 25ps LSB TDC
+  }
+  if (tdc_readings_[wcYd_].size()!=0 && tdc_readings_[wcYu_].size()!=0){
+    float TYd = *std::min_element(tdc_readings_[wcYd_].begin(),tdc_readings_[wcYd_].begin()+tdc_readings_[wcYd_].size());
+    float TYu = *std::min_element(tdc_readings_[wcYu_].begin(),tdc_readings_[wcYu_].begin()+tdc_readings_[wcYu_].size());
+    tdc_recoy_ = (TYu-TYd)*0.005; // = /40./5./10. //position in cm 0.2mm/ns with 25ps LSB TDC
+  }
+}
+
 
 //
 H4treeReco::~H4treeReco()
